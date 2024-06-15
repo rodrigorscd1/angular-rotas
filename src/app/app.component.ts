9b3bebb5f15422bda5beae8f0b5d37fa259243ec
RODRIGO DE SOUSA COBRA D ECA <rodrigo.scdeca@senacsp.edu.br>
2024-06-15 08:26:28 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './componentes/login/login.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, CommonModule, LoginComponent, ClientesComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'login', component: LoginComponent},
+    {path:'clientes', component: ClientesComponent},
+];
